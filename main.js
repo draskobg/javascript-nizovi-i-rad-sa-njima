c91238f489574a8635be97751405fbc8dec494e8
Drasko <draskosorak@gmail.com>
2018-12-14 19:29:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+proizvod = {
+    cena: 1000,
+    boja: "#00ff00",
+    tezina: 2,
+}
+
+
+niz = [
+    {
+        cena: 1000,
+        boja: "#00ff00",
+        tezina: 2,
+    },
+    {
+        cena: 700,
+        boja: "#ff000",
+        tezina: 2,
+    },
+    {
+        cena: 1200,
+        boja: "#0000ff=",
+        tezina: 2,
+    },
+    {
+        cena: 3000,
+        boja: "#d2ae77",
+        tezina: 2,
+    },
+]
---
+++ primer.js
@@ -0,0 +1,355 @@
+const korisnici = [
+    {
+        "_id": "5c13d4da36e2ff3b469ebd3a",
+        "index": 0,
+        "guid": "b342db56-4f6e-471f-8016-0370941b86a9",
+        "isActive": false,
+        "balance": "$2,516.96",
+        "picture": "http://placehold.it/32x32",
+        "age": 21,
+        "eyeColor": "green",
+        "name": "Cole Mcgee",
+        "gender": "male",
+        "company": "STRALUM",
+        "email": "colemcgee@stralum.com",
+        "phone": "+1 (955) 556-2146",
+        "address": "832 Gates Avenue, Chautauqua, Guam, 9857",
+        "about": "Tempor tempor do commodo culpa quis non non sunt qui dolore exercitation minim. Duis consequat ullamco ea ex deserunt sit dolor irure adipisicing duis. Labore esse est minim dolore aliqua. Qui irure reprehenderit aliqua mollit esse qui officia. Aliqua id deserunt occaecat aliquip est sint cillum magna et aliqua labore. Ea sint dolor cupidatat enim pariatur excepteur sit.\r\n",
+        "registered": "2018-05-01T06:56:34 -02:00",
+        "latitude": 53.713363,
+        "longitude": 60.632795,
+        "tags": [
+            "id",
+            "ea",
+            "culpa",
+            "duis",
+            "aliqua",
+            "non",
+            "officia"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Shannon Norton"
+            },
+            {
+                "id": 1,
+                "name": "Constance Burks"
+            },
+            {
+                "id": 2,
+                "name": "Susan Carter"
+            }
+        ],
+        "greeting": "Hello, Cole Mcgee! You have 4 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5c13d4da4d20f3ed8c2731dd",
+        "index": 1,
+        "guid": "a6685f3d-da18-40ee-919f-b05d51b90a73",
+        "isActive": false,
+        "balance": "$1,123.59",
+        "picture": "http://placehold.it/32x32",
+        "age": 26,
+        "eyeColor": "green",
+        "name": "Elsie Beard",
+        "gender": "female",
+        "company": "LETPRO",
+        "email": "elsiebeard@letpro.com",
+        "phone": "+1 (862) 457-2163",
+        "address": "866 Hemlock Street, Elliston, Federated States Of Micronesia, 4604",
+        "about": "Nulla dolore fugiat anim cupidatat non duis occaecat fugiat sunt duis officia sunt. Officia quis deserunt sit veniam ut ea aliquip anim do consectetur. Exercitation adipisicing ea nostrud ex minim velit sunt ad id. Ipsum consequat non est velit. Pariatur officia nisi nulla ex deserunt elit ullamco. Sunt do voluptate irure deserunt eu deserunt labore. Officia qui nisi ut do.\r\n",
+        "registered": "2015-05-22T10:12:11 -02:00",
+        "latitude": 64.058512,
+        "longitude": -173.055984,
+        "tags": [
+            "consectetur",
+            "laborum",
+            "ut",
+            "est",
+            "reprehenderit",
+            "quis",
+            "laborum"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Angel Nolan"
+            },
+            {
+                "id": 1,
+                "name": "Kirby Alexander"
+            },
+            {
+                "id": 2,
+                "name": "Valentine Richmond"
+            }
+        ],
+        "greeting": "Hello, Elsie Beard! You have 9 unread messages.",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5c13d4dad044a04b965e5577",
+        "index": 2,
+        "guid": "43ed2a60-353b-4e37-8c6f-e54bcb3d46b6",
+        "isActive": false,
+        "balance": "$2,287.09",
+        "picture": "http://placehold.it/32x32",
+        "age": 34,
+        "eyeColor": "brown",
+        "name": "Roy Myers",
+        "gender": "male",
+        "company": "ELEMANTRA",
+        "email": "roymyers@elemantra.com",
+        "phone": "+1 (830) 560-3487",
+        "address": "824 Dekalb Avenue, Levant, Iowa, 2610",
+        "about": "Laborum incididunt aute minim deserunt pariatur ullamco laborum officia. Esse ea elit culpa deserunt consectetur consectetur excepteur adipisicing laborum enim officia. Lorem tempor aute pariatur commodo incididunt irure dolore aliqua. Ex dolore proident in nulla occaecat. Id anim cillum sunt enim ipsum aute.\r\n",
+        "registered": "2017-02-03T04:20:08 -01:00",
+        "latitude": 89.314869,
+        "longitude": -66.434661,
+        "tags": [
+            "sit",
+            "aute",
+            "magna",
+            "incididunt",
+            "duis",
+            "dolor",
+            "amet"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Bridget Stephens"
+            },
+            {
+                "id": 1,
+                "name": "Lelia Roberts"
+            },
+            {
+                "id": 2,
+                "name": "Lakisha Oneal"
+            }
+        ],
+        "greeting": "Hello, Roy Myers! You have 8 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5c13d4da025081f7665db31b",
+        "index": 3,
+        "guid": "cf4c6f5f-afe8-4952-9a30-6301cd6931bb",
+        "isActive": true,
+        "balance": "$3,933.79",
+        "picture": "http://placehold.it/32x32",
+        "age": 37,
+        "eyeColor": "green",
+        "name": "Kerr Grimes",
+        "gender": "male",
+        "company": "ORBAXTER",
+        "email": "kerrgrimes@orbaxter.com",
+        "phone": "+1 (933) 442-3277",
+        "address": "371 Seton Place, Sutton, Alaska, 3765",
+        "about": "Est magna occaecat aute proident elit duis minim dolor. Pariatur eu voluptate ea anim excepteur elit. Aliquip aute ipsum minim aliquip adipisicing est proident ad elit. Irure consectetur sunt fugiat elit officia. Ut non aute adipisicing dolore.\r\n",
+        "registered": "2015-05-04T02:50:30 -02:00",
+        "latitude": 17.131866,
+        "longitude": -61.775815,
+        "tags": [
+            "reprehenderit",
+            "occaecat",
+            "pariatur",
+            "velit",
+            "cupidatat",
+            "sint",
+            "ipsum"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Amy Downs"
+            },
+            {
+                "id": 1,
+                "name": "Annette Carr"
+            },
+            {
+                "id": 2,
+                "name": "Holder Lane"
+            }
+        ],
+        "greeting": "Hello, Kerr Grimes! You have 1 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5c13d4dacb54a234d965f19e",
+        "index": 4,
+        "guid": "be08209f-5158-4f2d-ad95-205e8525e793",
+        "isActive": true,
+        "balance": "$3,671.20",
+        "picture": "http://placehold.it/32x32",
+        "age": 28,
+        "eyeColor": "brown",
+        "name": "Tamika Lloyd",
+        "gender": "female",
+        "company": "RUGSTARS",
+        "email": "tamikalloyd@rugstars.com",
+        "phone": "+1 (992) 453-2548",
+        "address": "400 Beadel Street, Chestnut, Wisconsin, 7200",
+        "about": "Amet eiusmod fugiat veniam id quis qui voluptate duis veniam incididunt proident reprehenderit duis reprehenderit. Do pariatur cupidatat ad non laborum commodo voluptate dolore occaecat. Sunt cillum ullamco tempor sint sunt officia tempor officia consectetur non eiusmod sit exercitation. Mollit culpa nisi eiusmod proident mollit. Consectetur eu commodo anim est adipisicing est nisi pariatur duis exercitation nisi sunt ipsum. Fugiat cupidatat commodo ex et qui laborum.\r\n",
+        "registered": "2014-07-03T11:05:44 -02:00",
+        "latitude": 2.144264,
+        "longitude": 38.620185,
+        "tags": [
+            "amet",
+            "voluptate",
+            "adipisicing",
+            "occaecat",
+            "officia",
+            "veniam",
+            "dolor"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Tracie Pickett"
+            },
+            {
+                "id": 1,
+                "name": "Barry Sawyer"
+            },
+            {
+                "id": 2,
+                "name": "Stuart Peters"
+            }
+        ],
+        "greeting": "Hello, Tamika Lloyd! You have 7 unread messages.",
+        "favoriteFruit": "banana"
+    },
+    {
+        "_id": "5c13d4da3de261f34951ae7e",
+        "index": 5,
+        "guid": "4e1b1c4b-9d0a-4760-995a-cc2df91015b0",
+        "isActive": false,
+        "balance": "$1,674.57",
+        "picture": "http://placehold.it/32x32",
+        "age": 23,
+        "eyeColor": "brown",
+        "name": "Battle Bush",
+        "gender": "male",
+        "company": "COMVEYOR",
+        "email": "battlebush@comveyor.com",
+        "phone": "+1 (931) 512-2765",
+        "address": "282 Havemeyer Street, Gouglersville, North Dakota, 5959",
+        "about": "Ipsum magna est aute excepteur aliqua minim quis exercitation sint qui enim Lorem ea. Sunt aliquip ea aliquip veniam elit sint sunt qui. Labore voluptate dolor consectetur dolore occaecat nulla dolor et mollit exercitation incididunt duis.\r\n",
+        "registered": "2015-12-06T07:24:49 -01:00",
+        "latitude": -17.596819,
+        "longitude": -127.321127,
+        "tags": [
+            "sunt",
+            "ea",
+            "voluptate",
+            "laborum",
+            "est",
+            "eiusmod",
+            "aliquip"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Meredith Hines"
+            },
+            {
+                "id": 1,
+                "name": "Gamble Price"
+            },
+            {
+                "id": 2,
+                "name": "Cox Lamb"
+            }
+        ],
+        "greeting": "Hello, Battle Bush! You have 10 unread messages.",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5c13d4dacfd97dc7f4ab0d40",
+        "index": 6,
+        "guid": "6e6f622c-cf0b-4b00-be96-86b8eb82fed2",
+        "isActive": false,
+        "balance": "$3,085.09",
+        "picture": "http://placehold.it/32x32",
+        "age": 22,
+        "eyeColor": "brown",
+        "name": "Griffith Guy",
+        "gender": "male",
+        "company": "ORBALIX",
+        "email": "griffithguy@orbalix.com",
+        "phone": "+1 (919) 545-3568",
+        "address": "140 Main Street, Buxton, New Mexico, 5152",
+        "about": "Eiusmod reprehenderit amet duis voluptate aliquip deserunt elit. Velit dolor ea aute nisi aute. Irure enim sunt esse minim.\r\n",
+        "registered": "2016-12-11T06:24:33 -01:00",
+        "latitude": 74.456267,
+        "longitude": 38.743932,
+        "tags": [
+            "cupidatat",
+            "minim",
+            "occaecat",
+            "nulla",
+            "reprehenderit",
+            "veniam",
+            "cupidatat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Luella Powers"
+            },
+            {
+                "id": 1,
+                "name": "Marian Richard"
+            },
+            {
+                "id": 2,
+                "name": "Frye Vincent"
+            }
+        ],
+        "greeting": "Hello, Griffith Guy! You have 4 unread messages.",
+        "favoriteFruit": "apple"
+    }
+]
+
+for (let i = 0; i < korisnici.length; i++) {
+    console.log(korisnici[i].name)
+}
+
+
+//
+let godine = 0;
+for (let i = 0; i < korisnici.length; i++) {
+    godine += korisnici[i].age
+}
+
+console.log(godine / korisnici.length)
+
+// prosecne godine u nizu
+
+let trazenoIme = "Kerr"
+let nadjeno = false
+
+for (let i = 0; i < korisnici.length; i++) {
+    if (korisnici[i].name.includes(trazenoIme)) nadjeno = true
+}
+
+if (nadjeno)
+    console.log("Nasli smo ime: " + trazenoIme)
+else
+    console.log("Nismo nasli ime")
+// da li postoji osoba sa imenom Tamika
+
+let ukupno = 0
+for (let i = 0; i < korisnici.length; i++) {
+    const plata = Number(korisnici[i].balance.replace("$","").replace(".",""))
+    ukupno += plata
+}
+const prosek = ukupno / korisnici.length
+console.log("Prosecna plata korisnika je:" + prosek.toFixed(2) + " dolara");
+
+// prosecna plata korisnika
